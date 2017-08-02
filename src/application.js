4bc1bd6a08f7304808394b77333f44a9e61d2e34
Developer Davo <DeveloperDavo@users.noreply.github.com>
2017-08-02 21:06:59 +0200

    Extract converting nodel list to array in application

--- src/application.js
+++ src/application.js
@@ -1,9 +1,14 @@
 'use strict';
 
+var convertNodeListToArray = function (nodeListElements) {
+    return Array.prototype.slice.call(nodeListElements);
+};
+
 function Application() {
     this.init = function () {
-        var numberElements = document.getElementsByClassName('number');
-        Array.prototype.slice.call(numberElements).forEach(function (numberElement) {
+        var numberElementsArray = convertNodeListToArray(document.getElementsByClassName('number'));
+
+        numberElementsArray.forEach(function (numberElement) {
             numberElement.onclick = (function (value) {
                 return function () {
                     document.getElementById('display').innerHTML = value;
