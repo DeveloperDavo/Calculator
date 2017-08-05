f9d57c0dea5fc364955ee6fb51facca62162795c
Developer Davo <DeveloperDavo@users.noreply.github.com>
2017-08-05 15:15:16 +0200

    Remove all display tests and rename 'display' to 'show' in history tests

--- spec/applicationSpec.js
+++ spec/applicationSpec.js
@@ -34,129 +34,7 @@ describe("application", function () {
         document.body.removeChild(container);
     });
 
-    it('should display value of 0 upon initialisation', function () {
-        application.init();
-
-        expect(document.getElementById('display').textContent).toEqual('0');
-    });
-
-    it('should display value of button 3', function () {
-        application.init();
-
-        document.getElementById('3').click();
-        expect(document.getElementById('display').textContent).toEqual('3');
-    });
-
-    it('should display value of button 9', function () {
-        application.init();
-
-        document.getElementById('9').click();
-        expect(document.getElementById('display').textContent).toEqual('9');
-    });
-
-    it('should display value of two consecutive numbers', function () {
-        application.init();
-
-        document.getElementById('7').click();
-        document.getElementById('5').click();
-
-        expect(document.getElementById('display').textContent).toEqual('75');
-    });
-
-    it('should display + operation', function () {
-        application.init();
-
-        document.getElementById('8').click();
-        document.getElementById('+').click();
-
-        expect(document.getElementById('display').textContent).toEqual('+');
-    });
-
-    it('should display current number after addition operation', function () {
-        application.init();
-
-        document.getElementById('2').click();
-        document.getElementById('+').click();
-        document.getElementById('4').click();
-
-        expect(document.getElementById('display').textContent).toEqual('4');
-    });
-
-    it('should display result of adding two numbers', function () {
-        application.init();
-
-        document.getElementById('2').click();
-        document.getElementById('0').click();
-        document.getElementById('+').click();
-        document.getElementById('1').click();
-        document.getElementById('0').click();
-        document.getElementById('equals').click();
-
-        expect(document.getElementById('display').textContent).toEqual('30');
-    });
-
-    it('should display result of subtracting one number from another', function () {
-        application.init();
-
-        document.getElementById('2').click();
-        document.getElementById('0').click();
-        document.getElementById('-').click();
-        document.getElementById('1').click();
-        document.getElementById('0').click();
-        document.getElementById('equals').click();
-
-        expect(document.getElementById('display').textContent).toEqual('10');
-    });
-
-    it('should display result of multiplying two numbers', function () {
-        application.init();
-
-        document.getElementById('2').click();
-        document.getElementById('*').click();
-        document.getElementById('3').click();
-        document.getElementById('equals').click();
-
-        expect(document.getElementById('display').textContent).toEqual('6');
-    });
-
-    it('should display result of dividing one number by another', function () {
-        application.init();
-
-        document.getElementById('6').click();
-        document.getElementById('/').click();
-        document.getElementById('3').click();
-        document.getElementById('equals').click();
-
-        expect(document.getElementById('display').textContent).toEqual('2');
-    });
-    it('should display result of multiple chained operations', function () {
-        application.init();
-
-        document.getElementById('2').click();
-        document.getElementById('0').click();
-        document.getElementById('-').click();
-        document.getElementById('1').click();
-        document.getElementById('0').click();
-        document.getElementById('+').click();
-        document.getElementById('6').click();
-        document.getElementById('equals').click();
-
-        expect(document.getElementById('display').textContent).toEqual('16');
-    });
-
-    it('should display new number after equals operation', function () {
-        application.init();
-
-        document.getElementById('2').click();
-        document.getElementById('+').click();
-        document.getElementById('4').click();
-        document.getElementById('equals').click();
-        document.getElementById('1').click();
-
-        expect(document.getElementById('display').textContent).toEqual('1');
-    });
-
-    it('should display new history after equals operation', function () {
+    it('should show new history after equals operation', function () {
         application.init();
 
         document.getElementById('2').click();
@@ -168,106 +46,8 @@ describe("application", function () {
         expect(document.getElementById('history').textContent).toEqual('1');
     });
 
-    it('should perform more than one operation', function () {
-        application.init();
 
-        document.getElementById('2').click();
-        document.getElementById('+').click();
-        document.getElementById('4').click();
-        document.getElementById('equals').click();
-
-        expect(document.getElementById('display').textContent).toEqual('6');
-
-        document.getElementById('1').click();
-        document.getElementById('-').click();
-        document.getElementById('1').click();
-        document.getElementById('equals').click();
-
-        expect(document.getElementById('display').textContent).toEqual('0');
-    });
-
-    it('should perform addition on existing result', function () {
-        application.init();
-
-        document.getElementById('2').click();
-        document.getElementById('+').click();
-        document.getElementById('4').click();
-        document.getElementById('equals').click();
-        document.getElementById('+').click();
-        document.getElementById('1').click();
-        document.getElementById('equals').click();
-
-        expect(document.getElementById('display').textContent).toEqual('7');
-    });
-
-    it('should display operator upon first click', function () {
-        application.init();
-
-        document.getElementById('+').click();
-        expect(document.getElementById('display').textContent).toEqual('+');
-
-        document.getElementById('-').click();
-        expect(document.getElementById('display').textContent).toEqual('-');
-
-        document.getElementById('*').click();
-        expect(document.getElementById('display').textContent).toEqual('*');
-
-        document.getElementById('/').click();
-        expect(document.getElementById('display').textContent).toEqual('/');
-    });
-
-    it('should display decimal number', function () {
-        application.init();
-
-        document.getElementById('decimal-point').click();
-        document.getElementById('3').click();
-        expect(document.getElementById('display').textContent).toEqual('0.3');
-
-        document.getElementById('-').click();
-        document.getElementById('2').click();
-        document.getElementById('decimal-point').click();
-        expect(document.getElementById('display').textContent).toEqual('2.');
-
-        document.getElementById('1').click();
-        expect(document.getElementById('display').textContent).toEqual('2.1');
-
-        document.getElementById('equals').click();
-        expect(document.getElementById('display').textContent).toEqual('-1.8');
-
-    });
-
-    it('should show result with decimal when dividing', function () {
-        application.init();
-
-        document.getElementById('2').click();
-        document.getElementById('/').click();
-        document.getElementById('3').click();
-        document.getElementById('equals').click();
-
-        expect(document.getElementById('display').textContent).toBeCloseTo('0.67', 2);
-    });
-
-    it('should not display 0 as the first digit of a whole number', function () {
-        application.init();
-
-        document.getElementById('0').click();
-        document.getElementById('2').click();
-
-        expect(document.getElementById('display').textContent).toBe('2');
-    });
-
-    it('should calculate result when operation is clicked before a number', function () {
-        application.init();
-
-        document.getElementById('-').click();
-        document.getElementById('3').click();
-        document.getElementById('equals').click();
-
-        expect(document.getElementById('display').textContent).toBe('-3');
-
-    });
-
-    it('should display 0 in history when operation is clicked before a number', function () {
+    it('should show 0 in history when operation is clicked before a number', function () {
         application.init();
 
         document.getElementById('-').click();
@@ -278,7 +58,7 @@ describe("application", function () {
 
     });
 
-    it('should display history', function () {
+    it('should show history', function () {
         application.init();
 
         expect(document.getElementById('history').textContent).toBe('0');
@@ -303,7 +83,7 @@ describe("application", function () {
 
     });
 
-    it('should display new history after reusing result', function () {
+    it('should show new history after reusing result', function () {
         application.init();
 
         document.getElementById('2').click();
@@ -336,53 +116,6 @@ describe("application", function () {
 
     });
 
-    it('should clear display after clicking clear all', function () {
-        application.init();
-
-        document.getElementById('2').click();
-        document.getElementById('/').click();
-        document.getElementById('3').click();
-        document.getElementById('all-clear').click();
-
-        expect(document.getElementById('display').textContent).toBe('0');
-
-        document.getElementById('2').click();
-        document.getElementById('/').click();
-        document.getElementById('3').click();
-
-        document.getElementById('equals').click();
-        expect(document.getElementById('display').textContent).toBeCloseTo('0.67', 2);
-    });
-
-    it('should still calculate result after clearing all first', function () {
-        application.init();
-
-        document.getElementById('all-clear').click();
-        document.getElementById('+').click();
-        document.getElementById('7').click();
-        document.getElementById('equals').click();
-
-        expect(document.getElementById('display').textContent).toBe('7');
-
-    });
-
-    it('should clear operation in display when clearing entry', function () {
-        application.init();
-
-        document.getElementById('2').click();
-        document.getElementById('/').click();
-        document.getElementById('clear-entry').click();
-
-        expect(document.getElementById('display').textContent).toBe('0');
-
-        document.getElementById('-').click();
-        document.getElementById('3').click();
-        document.getElementById('equals').click();
-
-        expect(document.getElementById('display').textContent).toBe('-1');
-
-    });
-
     it('should clear operation in history entry when clearing entry', function () {
         application.init();
 
@@ -400,26 +133,9 @@ describe("application", function () {
 
     });
 
-    it('should still display result when clearing an operation after equals', function () {
+    it('should still show history when clearing an operation after equals', function () {
         application.init();
 
-        document.getElementById('2').click();
-        document.getElementById('-').click();
-        document.getElementById('3').click();
-        document.getElementById('equals').click();
-        document.getElementById('-').click();
-        document.getElementById('clear-entry').click();
-        document.getElementById('+').click();
-        document.getElementById('2').click();
-        document.getElementById('equals').click();
-
-        expect(document.getElementById('display').textContent).toBe('1');
-
-    });
-
-    it('should still display history when clearing an operation after equals', function () {
-        application.init();
-        
         document.getElementById('2').click();
         document.getElementById('-').click();
         document.getElementById('3').click();
@@ -431,24 +147,6 @@ describe("application", function () {
         document.getElementById('equals').click();
 
         expect(document.getElementById('history').textContent).toBe('-1+2=1');
-    });
-
-    it('should clear digits in display entry when clearing entry', function () {
-        application.init();
-
-        document.getElementById('2').click();
-        document.getElementById('+').click();
-        document.getElementById('2').click();
-        document.getElementById('0').click();
-        document.getElementById('clear-entry').click();
-
-        expect(document.getElementById('display').textContent).toBe('0');
-
-        document.getElementById('4').click();
-        document.getElementById('0').click();
-        document.getElementById('equals').click();
-
-        expect(document.getElementById('display').textContent).toBe('42');
     });
 
     it('should clear digits in history entry when clearing entry', function () {
@@ -467,18 +165,6 @@ describe("application", function () {
         document.getElementById('equals').click();
 
         expect(document.getElementById('history').textContent).toBe('2+40=42');
-
-    });
-
-    it('should still calculate result after clearing entry first', function () {
-        application.init();
-
-        document.getElementById('clear-entry').click();
-        document.getElementById('+').click();
-        document.getElementById('7').click();
-        document.getElementById('equals').click();
-
-        expect(document.getElementById('display').textContent).toBe('7');
 
     });
 
